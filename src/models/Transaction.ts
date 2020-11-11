2325fc253c353c58ad8df2284057b47d43f2c83e
Patton Hoffiman <PattonHoffiman@gmail.com>
2020-11-10 22:24:45 -0300

    Adapt Models for use TypeORM with Postgree

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,39 @@
-class Transaction {
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
+export default class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @Column('uuid')
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
-
-export default Transaction;
