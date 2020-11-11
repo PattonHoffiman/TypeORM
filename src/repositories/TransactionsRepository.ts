007dd11dad83346041f18a3029bec3cb5f27dd72
Patton Hoffiman <PattonHoffiman@gmail.com>
2020-11-10 22:28:16 -0300

    Create a Custom Repository for Transactions Data

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,10 +9,34 @@ interface Balance {
 }
 
 @EntityRepository(Transaction)
-class TransactionsRepository extends Repository<Transaction> {
+export default class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const incomeArray = transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const outcomeArray = transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const income = incomeArray.reduce((acc, transaction) => {
+      return acc + Number(transaction.value);
+    }, 0);
+
+    const outcome = outcomeArray.reduce((acc, transaction) => {
+      return acc + Number(transaction.value);
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
+
+  public async getTransactions(): Promise<Transaction[]> {
+    const transactions = await this.find({
+      relations: ['category'],
+    });
+
+    return transactions;
   }
 }
-
-export default TransactionsRepository;
